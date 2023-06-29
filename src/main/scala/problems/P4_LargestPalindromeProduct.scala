8a9cf46e72fb421b5209bb5d955f9024a0d1e04e
artsiomchekh <qbartem@gmail.com>
2023-06-29 17:30:35 +0300

    P4: def isPalindrome

--- src/main/scala/problems/P4_LargestPalindromeProduct.scala
+++ src/main/scala/problems/P4_LargestPalindromeProduct.scala
@@ -6,16 +6,23 @@ package problems
  */
 
 object P4_LargestPalindromeProduct:
+  def isPalindrome(n: Int): Boolean =
+    var num = n
+    var rev = 0
+    while num > 0 do
+      val last = num % 10
+      rev = rev * 10 + last
+      num = num / 10
+    n == rev
+
   def largestPalindromeProduct(): Int =
     var largestPalindrome = 0
     for x: Int <- 100 until 999 by +1 do
       for y: Int <- 999 until 100 by -1 do
-        val prePal = x * y
-        if prePal % 10 == prePal % 1000000 / 100000
-          && prePal % 100 / 10 == prePal % 100000 / 10000
-          && prePal % 1000 / 100 == prePal % 10000 / 1000 then
-          if largestPalindrome < prePal then
-            largestPalindrome = prePal
+        val prePalindrome = x * y
+        if isPalindrome(prePalindrome) then
+          if largestPalindrome < prePalindrome then
+            largestPalindrome = prePalindrome
     largestPalindrome
 end P4_LargestPalindromeProduct
 
--- src/test/scala/problems/P4_LargestPalindromeProductTest.scala
+++ src/test/scala/problems/P4_LargestPalindromeProductTest.scala
@@ -1,7 +1,8 @@
 package problems
 
+import junit.framework.Assert.{assertFalse, assertTrue}
 import munit.FunSuite
-import problems.P4_LargestPalindromeProduct.largestPalindromeProduct
+import problems.P4_LargestPalindromeProduct.{isPalindrome, largestPalindromeProduct}
 
 class P4_LargestPalindromeProductTest extends FunSuite:
   test("121"):
@@ -12,4 +13,20 @@ class P4_LargestPalindromeProductTest extends FunSuite:
 
   test("largest Palindrome 906609"):
     assertEquals(largestPalindromeProduct(), 906609)
+
+  //isPalindrome
+  test("0"):
+    assertTrue(isPalindrome(0))
+
+  test("1"):
+    assertTrue(isPalindrome(1))
+
+  test("12"):
+    assertFalse(isPalindrome(12))
+
+  test("121221"):
+      assertFalse(isPalindrome(121221))
+
+  test("906609"):
+      assertTrue(isPalindrome(906609))
 
