bafeb6cb746c2780cf6edd9151b1dcb73e6dd54c
artsiomchekh <qbartem@gmail.com>
2023-07-06 10:38:05 +0300

    P5_SmallestMultiple 18044195 wrong answer

--- src/main/scala/problems/P5_SmallestMultiple.scala
+++ src/main/scala/problems/P5_SmallestMultiple.scala
@@ -1,7 +1,23 @@
 package problems
 
-object P5_SmallestMultiple {
-  def smallestMultiple(a: Int, b: Int): Int = ???
+/*
+ * Smallest Multiple
+ * https://projecteuler.net/problem=5
+ */
+object P5_SmallestMultiple:
+  def gcd(a: Int, b: Int): Int =
+    if b == 0 then
+      a
+    else
+      gcd(b, a % b)
 
+  def lcm(a: Int, b: Int): Int =
+    a * b / gcd(a, b)
 
-}
+  def smallestMultiple(a: Int, b: Int): Int =
+    var res = a
+    for i <- a + 1 to b do
+      res = lcm(res, i)
+    res
+end P5_SmallestMultiple
+
--- src/test/scala/problems/P5_SmallestMultipleTest.scala
+++ src/test/scala/problems/P5_SmallestMultipleTest.scala
@@ -1,10 +1,27 @@
 package problems
 
 import munit.FunSuite
-import problems.P5_SmallestMultiple.smallestMultiple
+import problems.P5_SmallestMultiple.{gcd, lcm, smallestMultiple}
 
 class P5_SmallestMultipleTest extends FunSuite:
-  test("test 1"):
+
+  test("gcd"):
+    assertEquals(gcd(1, 2), 1)
+    assertEquals(gcd(4, 6), 2)
+    assertEquals(gcd(5, 7), 1)
+    assertEquals(gcd(9, 12), 3)
+
+  test("lcm"):
+    assertEquals(lcm(1, 2), 2)
+    assertEquals(lcm(3, 5), 15)
+    assertEquals(lcm(5, 7), 35)
+    assertEquals(lcm(9, 12), 36)
+
+  test("smallestMultiple"):
     assertEquals(smallestMultiple(1, 2), 2)
+    assertEquals(smallestMultiple(1, 5), 60)
+    assertEquals(smallestMultiple(10, 12), 660)
+    assertEquals(smallestMultiple(1, 10), 2520)
+    assertEquals(smallestMultiple(1, 20), 18044195)
 
 
