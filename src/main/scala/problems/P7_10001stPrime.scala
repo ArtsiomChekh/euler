514da6d1f5a0f932bbbb0ee7b9fb65ff648baf7a
artsiomchekh <qbartem@gmail.com>
2023-07-14 18:18:46 +0300

    P7_10001stPrime: isPrime update

--- src/main/scala/problems/P7_10001stPrime.scala
+++ src/main/scala/problems/P7_10001stPrime.scala
@@ -9,14 +9,14 @@ import scala.annotation.tailrec
 object P7_10001stPrime:
   def nthPrimeNumber(n: Int): Int =
     var counter = 1
-    var currPrime = 2
-    var res = 0
+    var candidate = 2
+    var prime = 0
     while counter <= n do
-      if isPrime(currPrime) then
-        res = currPrime
+      if isPrime(candidate) then
+        prime = candidate
         counter += 1
-      currPrime += 1
-    res
+      candidate += 1
+    prime
 
   def nthPrimeNumberByRecursion(n: Int): Int =
     @tailrec
@@ -30,12 +30,15 @@ object P7_10001stPrime:
 
     loop(2, 2, 1)
 
-  @tailrec
-  def isPrime(n: Int, i: Int = 2): Boolean =
-    if n == 0 || n == 1 then
-      false
-    else if n == i then
-      true
-    else if n % i == 0 then
-      false
-    else isPrime(n, i + 1)
+  def isPrime(n: Int): Boolean =
+    @tailrec
+    def loop(i: Int): Boolean =
+      if n == 2 || i * i > n then
+        true
+      else if n % i == 0 then
+        false
+      else
+        loop(i + 1)
+
+    loop(2)
+end P7_10001stPrime
