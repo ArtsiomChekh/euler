4e840e2730185add48a39de62c48ec00a8f06613
artsiomchekh <qbartem@gmail.com>
2023-07-17 16:54:25 +0300

    P8_LargestProductInASeries: def largestProductInASeries + test

--- src/main/scala/problems/P8_LargestProductInASeries.scala
+++ src/main/scala/problems/P8_LargestProductInASeries.scala
@@ -4,15 +4,14 @@ package problems
  * Largest Product in a Series
  * https://projecteuler.net/problem=8
  */
-
 object P8_LargestProductInASeries:
-  def largestProductInASeries(str: String, n: Int): Int =
-    var largest = 0
+  def largestProductInASeries(str: String, n: Int): Long =
+    var largest: Long = 0
     for i <- 0 to str.length - n do
-      var product = 1
+      var product: Long = 1
       for j <- 0 until n do
         product *= str.charAt(i + j).asDigit
       if largest < product then
         largest = product
-        println(largest)
     largest
+end P8_LargestProductInASeries
