0c5dca70a8deb7bbd407f75bb053b8d7bfef03ad
artsiomchekh <qbartem@gmail.com>
2023-07-27 15:18:20 +0300

    P10_SummationOfPrimes: added def sumOfPrimesBelowByRecursive + test

--- src/main/scala/problems/P10_SummationOfPrimes.scala
+++ src/main/scala/problems/P10_SummationOfPrimes.scala
@@ -1,5 +1,7 @@
 package problems
 
+import scala.annotation.tailrec
+
 /*
  * Summation of Primes
  * https://projecteuler.net/problem=10
@@ -11,4 +13,16 @@ object P10_SummationOfPrimes:
       if isPrime(i) && i < n then
         sum += i
     sum
+
+  def sumOfPrimesBelowByRecursive(n: Int): Long =
+    @tailrec
+    def loop(candidate: Int, sum: Long): Long =
+      if candidate >= n then
+        sum
+      else if isPrime(candidate) then
+        loop(candidate + 1, sum + candidate)
+      else
+        loop(candidate + 1, sum)
+
+    loop(2, 0)
 end P10_SummationOfPrimes
--- src/test/scala/problems/P10_SummationOfPrimesTest.scala
+++ src/test/scala/problems/P10_SummationOfPrimesTest.scala
@@ -23,3 +23,15 @@ class P10_SummationOfPrimesTest extends FunSuite:
 
   test("below 2 mln"):
     assertEquals(sumOfPrimesBelow(2000000), 142913828922L)
+
+  test("sumOfPrimesBelowByRecursive 3"):
+    assertEquals(sumOfPrimesBelowByRecursive(3), 2L)
+
+  test("sumOfPrimesBelowByRecursive 10"):
+    assertEquals(sumOfPrimesBelowByRecursive(10), 2 + 3 + 5 + 7L)
+
+  test("sumOfPrimesBelowByRecursive 100"):
+    assertEquals(sumOfPrimesBelowByRecursive(100), 1060L)
+
+  test("sumOfPrimesBelowByRecursive 2 mln"):
+    assertEquals(sumOfPrimesBelowByRecursive(2000000), 142913828922L)
