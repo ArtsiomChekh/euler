fef3a1e2ea4fcf91553524085a92f378b89f79cb
artsiomchekh <qbartem@gmail.com>
2023-08-17 16:07:47 +0300

    add P14_LongestCollatzSequence

--- src/main/scala/problems/P14_LongestCollatzSequence.scala
+++ src/main/scala/problems/P14_LongestCollatzSequence.scala
@@ -1,12 +1,41 @@
 package problems
 
+import scala.annotation.tailrec
+
 /*
  * Longest Collatz Sequence
  * https://projecteuler.net/problem=14
  */
 object P14_LongestCollatzSequence:
-  def produceChain(n: Int): Int =
-    1
+  def produceChain(n: Int): List[Long] =
+    @tailrec
+    def loop(current: Long, chain: List[Long]): List[Long] = current match
+      case 1 => current +: chain
+      case x if x % 2 == 0 => loop(current / 2, x +: chain)
+      case _ => loop(current * 3 + 1, current +: chain)
+
+    loop(n, Nil).reverse
+
+  def numberWithLongestChain(n: Int): Int =
+    var maxLength = 0
+    var numberWithMaxLength = 0
+
+    for i <- n to 1 by -1 do
+      val chainLength = produceChain(i).length
+      if chainLength > maxLength then
+        maxLength = chainLength
+        numberWithMaxLength = i
+
+    numberWithMaxLength
+end P14_LongestCollatzSequence
+    
+
+
+
+
+
+
+
 
 
 
--- src/test/scala/problems/P14_LongestCollatzSequenceTest.scala
+++ src/test/scala/problems/P14_LongestCollatzSequenceTest.scala
@@ -7,6 +7,20 @@ class P14_LongestCollatzSequenceTest extends FunSuite:
   import P14_LongestCollatzSequence.*
 
   test("test produceChain"):
-    assertEquals(produceChain(1), 1)
-//    assertEquals(produceChain(10), 7)
+    assertEquals(produceChain(1), List(1L))
+    assertEquals(produceChain(10), List(10L, 5L, 16L, 8L, 4L, 2L, 1L))
+    assertEquals(produceChain(13), List(13L, 40L, 20L, 10L, 5L, 16L, 8L, 4L, 2L, 1L))
+
+  test("test numberWithLongestChain 1, 2 , 9"):
+    assertEquals(numberWithLongestChain(1), 1)
+    assertEquals(numberWithLongestChain(2), 2)
+    assertEquals(numberWithLongestChain(10), 9)
+
+  test("test numberWithLongestChain 100 - 1 mln"):
+    assertEquals(numberWithLongestChain(100), 97)
+    assertEquals(numberWithLongestChain(1000), 871)
+    assertEquals(numberWithLongestChain(100000), 77031)
+    assertEquals(numberWithLongestChain(1000000), 837799)
+
+
 
