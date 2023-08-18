00e87efb3266e60c0d892433549def7cc9c8ccd8
artsiomchekh <qbartem@gmail.com>
2023-08-18 15:56:55 +0300

    P16_PowerDigitSum: added def powerDigitSum and test

--- src/main/scala/problems/P16_PowerDigitSum.scala
+++ src/main/scala/problems/P16_PowerDigitSum.scala
@@ -1,7 +1,24 @@
 package problems
 
+import scala.annotation.tailrec
+
+/*
+ * Power Digit Sum
+ * https://projecteuler.net/problem=16
+ */
 object P16_PowerDigitSum:
-  def powerDigitSum(n: Int): Int =
-    0
+  def powerDigitSum(n: Int, power: Int): Int =
+    @tailrec
+    def loop(acc: BigInt, remainingPower: Int, digitSum: Int): Int =
+      if remainingPower < 1 then
+        acc.toString.map(_.asDigit).sum
+      else
+        loop(acc * BigInt(n), remainingPower - 1, digitSum)
+
+    loop(BigInt(n), power - 1, 0)
+end P16_PowerDigitSum
+
+
+
 
 
--- src/test/scala/problems/P16_PowerDigitSumTest.scala
+++ src/test/scala/problems/P16_PowerDigitSumTest.scala
@@ -6,5 +6,23 @@ class P16_PowerDigitSumTest extends FunSuite:
 
   import P16_PowerDigitSum.*
 
-  test("powerDigitSum"):
-    assertEquals(powerDigitSum(0), 0)
+  test("powerDigitSum power to 15"):
+    assertEquals(powerDigitSum(1, 1), 1)
+    assertEquals(powerDigitSum(2, 1), 2)
+    assertEquals(powerDigitSum(3, 2), 9)
+    assertEquals(powerDigitSum(3, 3), 9)
+    assertEquals(powerDigitSum(2, 15), 26)
+
+  test("powerDigitSum power 100"):
+    assertEquals(powerDigitSum(2, 100), 115)
+
+  test("powerDigitSum power 1000"):
+    assertEquals(powerDigitSum(2, 1000), 1366)
+
+
+
+
+
+
+
+
