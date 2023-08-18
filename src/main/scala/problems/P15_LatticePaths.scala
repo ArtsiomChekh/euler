9826b155d7d7e6427fb21d2c829a43ef6e96cd6d
artsiomchekh <qbartem@gmail.com>
2023-08-18 11:02:19 +0300

    added P15_LatticePaths def routesCount

--- src/main/scala/problems/P15_LatticePaths.scala
+++ src/main/scala/problems/P15_LatticePaths.scala
@@ -5,4 +5,9 @@ package problems
  * https://projecteuler.net/problem=15
  */
 object P15_LatticePaths:
+  def routesCount(gridSize: Int): BigInt =
+    val n = gridSize * 2
+    val k = gridSize
 
+    bigFactorial(n) / (bigFactorial(k) * bigFactorial(n - k))
+end P15_LatticePaths
--- src/test/scala/problems/P15_LatticePathsTest.scala
+++ src/test/scala/problems/P15_LatticePathsTest.scala
@@ -4,3 +4,17 @@ import munit.FunSuite
 
 class P15_LatticePathsTest extends FunSuite:
 
+  import P15_LatticePaths.*
+
+  val grid2X2: Seq[Seq[Int]] = Seq.fill(2, 2)(0)
+  val grid4X4: Seq[Seq[Int]] = Seq.fill(4, 4)(0)
+  val grid20X20: Seq[Seq[Int]] = Seq.fill(20, 20)(0)
+
+  test("routesCount 2X2"):
+    assertEquals(routesCount(grid2X2.size), BigInt(6))
+
+  test("routesCount grid4X4"):
+    assertEquals(routesCount(grid4X4.size), BigInt(70))
+
+  test("routesCount grid20X20"):
+    assertEquals(routesCount(grid20X20.size), BigInt("137846528820"))
