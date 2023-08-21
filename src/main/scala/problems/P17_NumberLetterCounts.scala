8742fb72f3fb6853e65155257b08ec7b6d9c5154
artsiomchekh <qbartem@gmail.com>
2023-08-21 11:36:03 +0300

    P17_NumberLetterCounts: test 1000 error

---
+++ src/main/scala/problems/P17_NumberLetterCounts.scala
@@ -0,0 +1,42 @@
+package problems
+
+/*
+ * Number Letter Counts
+ * https://projecteuler.net/problem=17
+ */
+object P17_NumberLetterCounts:
+  private val units = Map("units" -> Array("zero", "one", "two", "three", "four", "five", "six", "seven",
+    "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+    "seventeen", "eighteen", "nineteen"))
+  private val tens = Map("tens" -> Array("", "ten", "twenty", "thirty", "forty", "fifty", "sixty",
+    "seventy", "eighty", "ninety"))
+
+  def numberLetterCounts(n: Int): Int =
+    var stringNumb = ""
+    for i <- 1 to n do
+      println(stringNumb)
+      var currentNumb = i
+
+      if currentNumb / 1000 > 0 then
+        stringNumb += units("units")(currentNumb / 1000) + "thousand"
+        currentNumb = currentNumb % 1000
+
+      if currentNumb / 100 > 0 then
+        stringNumb += units("units")(currentNumb / 100) + "hundred"
+        currentNumb = currentNumb % 100
+        if currentNumb > 0 then
+          stringNumb += "and"
+
+      if currentNumb < 20 then
+        stringNumb += units("units")(currentNumb)
+      else
+        stringNumb += tens("tens")(currentNumb / 10)
+        currentNumb = currentNumb % 10
+        if currentNumb > 0 then
+          stringNumb += units("units")(currentNumb)
+
+    stringNumb.length
+
+
+
+
---
+++ src/test/scala/problems/P17_NumberLetterCountsTest.scala
@@ -0,0 +1,23 @@
+package problems
+
+import munit.FunSuite
+
+class P17_NumberLetterCountsTest extends FunSuite:
+
+  import P17_NumberLetterCounts.*
+
+  test("numberLetterCounts to 1"):
+    assertEquals(numberLetterCounts(1), 3)
+
+  test("numberLetterCounts to 2"):
+    assertEquals(numberLetterCounts(2), 6)
+
+  test("numberLetterCounts to 5"):
+    assertEquals(numberLetterCounts(5), 19)
+
+  test("numberLetterCounts to 100"):
+    assertEquals(numberLetterCounts(100), 868)
+
+//  test("numberLetterCounts to 1000"):
+//    assertEquals(numberLetterCounts(1000), 21168)
+
