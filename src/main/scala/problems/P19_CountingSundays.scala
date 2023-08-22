8a18ca8ea230b4dbc941e7d0f512ad7eddc39ed2
artsiomchekh <qbartem@gmail.com>
2023-08-22 09:42:02 +0300

    P19_CountingSundays: added isLeapYear and test

--- src/main/scala/problems/P19_CountingSundays.scala
+++ src/main/scala/problems/P19_CountingSundays.scala
@@ -4,3 +4,6 @@ object P19_CountingSundays:
   def countSundays(firstYear: Int, lastYear: Int): Int =
     23
 
+  def isLeapYear(n: Int): Boolean =
+    n % 4 == 0 && n % 100 != 0 ||
+    n % 100 == 0 && n % 400 == 0
--- src/test/scala/problems/P19_CountingSundaysTest.scala
+++ src/test/scala/problems/P19_CountingSundaysTest.scala
@@ -8,3 +8,15 @@ class P19_CountingSundaysTest extends FunSuite:
 
   test("countSundays"):
     assertEquals(countSundays(1901, 1902), 23)
+
+  test("isLeapYear 2000"):
+    assert(isLeapYear(2000))
+
+  test("isLeapYear 2024"):
+    assert(isLeapYear(2024))
+
+  test("isLeapYear 1900"):
+    assert(!isLeapYear(1900))
+
+  test("isLeapYear 1901"):
+    assert(!isLeapYear(1901))
