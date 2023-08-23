f95caf93a27362372622a56a778a1413c69b295f
artsiomchekh <qbartem@gmail.com>
2023-08-23 15:49:59 +0300

    P22_NamesScores: add sumOfNamesScore + test

--- src/main/scala/problems/P22_NamesScores.scala
+++ src/main/scala/problems/P22_NamesScores.scala
@@ -5,8 +5,8 @@ package problems
  * https://projecteuler.net/problem=22
  */
 object P22_NamesScores:
-  def sumOfNamesScore() =
-    true
+  def sumOfNamesScore(array: Array[String]): Int =
+    array.indices.foldLeft(0)((acc, s) => acc + ((s + 1) * nameCharSum(array(s))))
 
   def nameCharSum(name: String): Int =
     name.toCharArray.foldLeft(0)((acc, c) => acc + (c - ('A' - 1)))
--- src/test/scala/problems/P22_NamesScoresTest.scala
+++ src/test/scala/problems/P22_NamesScoresTest.scala
@@ -5,22 +5,28 @@ import munit.FunSuite
 import scala.io.{BufferedSource, Source}
 
 class P22_NamesScoresTest extends FunSuite:
-
   val filePath = "src/main/resources/db/P22_names.txt"
   val source: BufferedSource = Source.fromFile(filePath)
-  val names: Array[String] = source.getLines.mkString.split(',').sorted
-  source.close
+  val names: Array[String] = source.getLines().flatMap(_.replaceAll("\"", "").split(",")).toSeq.sorted.toArray
+
+  val testNames: Array[String] = Array("ABC", "DAD", "AD")
 
   import P22_NamesScores._
 
-  test("sumOfNames"):
-    assert(sumOfNamesScore())
+  test("sumOfNamesScore testNames"):
+    assertEquals(sumOfNamesScore(testNames), 39)
+
+  test("sumOfNamesScore names"):
+    assertEquals(sumOfNamesScore(names), 871198282)
 
   test("nameCharSum ABC"):
-    assertEquals(nameCharSum("ABC"), 1+2+3)
+    assertEquals(nameCharSum("ABC"), 1 + 2 + 3)
 
   test("nameCharSum MARY"):
     assertEquals(nameCharSum("MARY"), 57)
 
   test("nameCharSum COLIN"):
     assertEquals(nameCharSum("COLIN"), 53)
+
+  test("nameCharSum BARBARA"):
+    assertEquals(nameCharSum("BARBARA"), 43)
