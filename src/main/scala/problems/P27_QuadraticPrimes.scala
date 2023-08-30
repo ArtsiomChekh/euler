e8105f6c6c416f076010387d9ce7697b261a1d7c
artsiomchekh <qbartem@gmail.com>
2023-08-30 09:40:28 +0300

    P27_QuadraticPrimes: add productOfTheCoefficients + test

--- src/main/scala/problems/P27_QuadraticPrimes.scala
+++ src/main/scala/problems/P27_QuadraticPrimes.scala
@@ -5,14 +5,28 @@ package problems
  * https://projecteuler.net/problem=27
  */
 object P27_QuadraticPrimes:
+  def productOfTheCoefficients(limit: Int): Int =
+    var max = 0
+    var maxA = 0
+    var maxB = 0
+    for a <- -limit until limit do
+      for b <- -limit to limit do
+        val count = primesCounter(a, b)
+        if count > max then
+          max = count
+          maxA = a
+          maxB = b
+    maxA * maxB
+
   def primesCounter(a: Int, b: Int): Int =
-    val result = (0 to Int.MaxValue).takeWhile(n => isPrime(quadraticForm(n, a, b)))
-    result.length
-
-  def quadraticForm(n: Int, a: Int, b: Int): Int =
-    n * n + a * n + b
-  
-
+    var i = 0
+    while true do
+      val n = i * i + i * a + b
+      if n < 0 || !isPrime(n) then
+        return i
+      i += 1
+    0
+end P27_QuadraticPrimes
 
 
 
--- src/test/scala/problems/P27_QuadraticPrimesTest.scala
+++ src/test/scala/problems/P27_QuadraticPrimesTest.scala
@@ -9,5 +9,8 @@ class P27_QuadraticPrimesTest extends FunSuite:
   test("primesCounter 40 primes"):
     assertEquals(primesCounter(1, 41), 40)
 
-  test("quadraticForm"):
-    assertEquals(quadraticForm(3, 1, 5), 17)
+  test("primesCounter 80 primes"):
+    assertEquals(primesCounter(-79, 1601), 80)
+
+  test("productOfTheCoefficients"):
+    assertEquals(productOfTheCoefficients(1000), -59231)
