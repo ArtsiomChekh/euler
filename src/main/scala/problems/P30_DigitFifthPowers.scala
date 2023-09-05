455b5609b94486c8b532d9ea2cd727adff4bfe56
artsiomchekh <qbartem@gmail.com>
2023-09-05 13:57:17 +0300

    P30_DigitFifthPowers: sumPowerDigits and digitNthPowers

--- src/main/scala/problems/P30_DigitFifthPowers.scala
+++ src/main/scala/problems/P30_DigitFifthPowers.scala
@@ -1,8 +1,30 @@
 package problems
 
-object P30_DigitFifthPowers:
+import scala.annotation.tailrec
 
-  def sumOfDigitNthPowers(power: Int): Int =
-    19316
+/*
+ * Digit Fifth Powers
+ * https://projecteuler.net/problem=30
+ */
+object P30_DigitFifthPowers:
+  def sumPowerDigits(value: Int, power: Int): Int =
+    @tailrec
+    def loop(acc: Int, remainder: Int): Int =
+      if remainder == 0 then
+        acc
+      else
+        loop(acc + Math.pow(remainder % 10, power).toInt, remainder / 10)
+
+    loop(0, value)
+
+  def digitNthPowers(end: Int, power: Int): Int =
+    var solution = 0
+    for numb <- 2 to end do
+      val temp = sumPowerDigits(numb, power)
+      if temp == numb then
+        solution += numb
+    solution
+end P30_DigitFifthPowers
+
 
 
--- src/test/scala/problems/P30_DigitFifthPowersTest.scala
+++ src/test/scala/problems/P30_DigitFifthPowersTest.scala
@@ -6,6 +6,15 @@ class P30_DigitFifthPowersTest extends FunSuite:
 
   import P30_DigitFifthPowers._
 
-  test("test 1"):
-    assertEquals(sumOfDigitNthPowers(4), 19316)
+  test("sumPowerDigits power 2"):
+    assertEquals(sumPowerDigits(22, 2), 8)
+
+  test("sumPowerDigits power 3"):
+    assertEquals(sumPowerDigits(22, 3), 16)
+
+  test("digitNthPowers 4"):
+    assertEquals(digitNthPowers(26244, 4), 19316)
+
+  test("digitNthPowers 5"):
+    assertEquals(digitNthPowers(295245, 5), 443839)
 
