ded64f223347fdfb74f58907ea49090f0e3615ac
artsiomchekh <qbartem@gmail.com>
2023-09-13 11:48:10 +0300

    P31_CoinSums: update

--- src/test/scala/problems/P31_CoinSumsTest.scala
+++ src/test/scala/problems/P31_CoinSumsTest.scala
@@ -6,8 +6,14 @@ class P31_CoinSumsTest extends FunSuite:
 
   import P31_CoinSums.*
 
-  val coins: Array[Int] = Array(1 , 2 , 5)
+  val coins: Array[Int] = Array(1 , 2 , 5, 10, 20, 50, 100, 200)
 
-  test("test"):
-    assertEquals(waysToMakeCoin(5, 2, coins), 4)
-//    assertEquals(waysToMakeCoin(10), 9)
+  test("test 5"):
+    assertEquals(countChange(5, coins), 4)
+
+  test("test 10"):
+    assertEquals(countChange(10, coins), 11)
+
+  test("test 200"):
+    assertEquals(countChange(200, coins), 73682)
+
