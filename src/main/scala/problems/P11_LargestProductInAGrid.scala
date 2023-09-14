8f026b67ab560deecad1f9ac9f56c3c649e999cb
artsiomchekh <qbartem@gmail.com>
2023-09-14 11:33:13 +0300

    P11_LargestProductInAGrid: update

--- src/main/scala/problems/P11_LargestProductInAGrid.scala
+++ src/main/scala/problems/P11_LargestProductInAGrid.scala
@@ -7,32 +7,27 @@ package problems
 object P11_LargestProductInAGrid:
   def descendingDiagonal(grid: Array[Array[Int]], r: Int, c: Int, n: Int): Seq[Int] =
     (0 until n).foldLeft(Seq[Int]())((acc, i) =>
-      acc :+ (if (r + i < grid.length && c + i < grid(r + i).length) grid(r + i)(c + i) else 0)
-    )
+      acc :+ (if (r + i < grid.length && c + i < grid(r + i).length) grid(r + i)(c + i) else 0))
 
   def ascendingDiagonal(grid: Array[Array[Int]], r: Int, c: Int, n: Int): Seq[Int] =
     (0 until n).foldLeft(Seq[Int]())((acc, i) =>
-      acc :+ (if (r - i >= 0 && c + i < grid(r - i).length) grid(r - i)(c + i) else 0)
-    )
+      acc :+ (if (r - i >= 0 && c + i < grid(r - i).length) grid(r - i)(c + i) else 0))
 
   def vertical(grid: Array[Array[Int]], r: Int, c: Int, n: Int): Seq[Int] =
     (0 until n).foldLeft(Seq[Int]())((acc, i) =>
-      acc :+ grid(r + i)(c)
-    )
+      acc :+ grid(r + i)(c))
 
   def horizontal(grid: Array[Array[Int]], r: Int, c: Int, n: Int): Seq[Int] =
     (0 until n).foldLeft(Seq[Int]())((acc, i) =>
-      acc :+ grid(r)(c + i)
-    )
+      acc :+ grid(r)(c + i))
 
   def maxProduct(grid: Array[Array[Int]], n: Int): Int =
     var listOfMax = Seq[Int]()
     for r <- 0 to grid.length - n
         c <- 0 to grid(0).length - n do
-      val new_max = List(horizontal(grid, r, c, n), vertical(grid, r, c, n),
+      val newMax = List(horizontal(grid, r, c, n), vertical(grid, r, c, n),
         descendingDiagonal(grid, r, c, n), ascendingDiagonal(grid, r, c, n)
       ).map(_.product).max
-      listOfMax = listOfMax :+ new_max
+      listOfMax = listOfMax :+ newMax
     listOfMax.max
 end P11_LargestProductInAGrid
-
