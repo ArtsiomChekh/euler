7dfa8dced268765293dd8ba685863ae10709e015
artsiomchekh <qbartem@gmail.com>
2023-09-14 14:04:27 +0300

    P31_CoinSums: update

--- src/main/scala/problems/P31_CoinSums.scala
+++ src/main/scala/problems/P31_CoinSums.scala
@@ -8,12 +8,12 @@ import scala.Array.ofDim
  */
 object P31_CoinSums:
   def countChange(money: Int, coins: Array[Int]): Int =
-    if money == 0 then
-      1
-    else if money < 0 || coins.isEmpty then
-      0
-    else
-      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    val ways = Array.fill(money + 1)(0)
+    ways(0) = 1
+    for i <- coins do
+      for k <- i to money do
+        ways(k) += ways(k - i)
+    ways(money)
 end P31_CoinSums
 
 
